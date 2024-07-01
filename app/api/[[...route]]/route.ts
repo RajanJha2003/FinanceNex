5d728c3326e1d795d00f134f4b3356c07258998f
RajanJha2003 <rajanj448@student.sfit.ac.in>
2024-07-01 17:39:43 +0530

    Tanstack Query

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,27 +2,18 @@ import {Hono} from 'hono'
 import {handle} from 'hono/vercel'
 import {zValidator} from '@hono/zod-validator'
 import { z } from 'zod';
-
+import accounts from "./accounts"
 export const runtime = 'edge';
 
 const app=new Hono().basePath("/api");
 
-app.get("/hello",(c)=>{
-    return c.json({
-        message:"Hello Finance"
-    })
-}).get("/hello/:test",zValidator("param",z.object({
-    test:z.number()
-})),(c)=>{
-   
-    return c.json({
-        message:"Hello World",
-       
-    })
-})
+const routes=app.route("/accounts",accounts);
 
 
 export const GET = handle(app);
 export const POST = handle(app);
 export const PATCH = handle(app);
 export const DELETE = handle(app);
+
+
+export type AppType=typeof routes;
