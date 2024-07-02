88296d0aaa67150974b3b93cdbc9593f8b2e68dd
Rajan Jha <rajanj448@student.sfit.ac.in>
2024-07-02 18:18:20 +0530

    Account Creation Completed

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -2,10 +2,38 @@ import React from 'react'
 
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from '@/components/ui/sheet';
 import { useNewAccount } from '../hooks/use-new-account';
+import AccountForm from './account-form';
+import { insertAccountSchema } from '@/db/schema';
+import { z } from 'zod';
+import { useCreateAccount } from '../api/use-create-account';
+
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
 
 const NewAccountSheet = () => {
 
-    const { isOpen, onClose } = useNewAccount();
+  const { isOpen, onClose } = useNewAccount();
+
+  const mutation=useCreateAccount();
+
+
+
+
+  const onSubmit=(values:FormValues)=>{
+    mutation.mutate(values,{
+      onSuccess:()=>{
+        onClose();
+      }
+    })
+
+
+  }
+
+    
   return (
    <Sheet open={isOpen} onOpenChange={onClose}>
     <SheetContent>
@@ -17,6 +45,9 @@ const NewAccountSheet = () => {
             Create a new account to track your transactions
           </SheetDescription>
         </SheetHeader>
+        <AccountForm onSubmit={onSubmit} defaultValues={{
+          name:""
+        }} />
     </SheetContent>
 
    </Sheet>
